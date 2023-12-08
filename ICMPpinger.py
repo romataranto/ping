1e7088d418af421cc13566e1dcff9bd41587cbf8
Roma Taranto <rtaranto@smith.edu>
2023-12-07 22:18:46 -0500

    Commented throughout.

--- ICMPpinger.py
+++ ICMPpinger.py
@@ -1,8 +1,6 @@
 # Attribution: this assignment is based on ICMP Pinger Lab from Computer Networking: a Top-Down Approach by Jim Kurose and Keith Ross. 
 # It was modified for use in CSC249: Networks at Smith College by R. Jordan Crouser in Fall 2022, and by Brant Cheikes for Fall 2023.
 
-# test
-
 from socket import * 
 import os
 import sys 
@@ -22,27 +20,37 @@ ICMP_ECHO_REQUEST = 8
 # You do not need to modify this method
 # -------------------------------------
 def checksum(string): 
+    #init variables
     csum = 0
-    countTo = (len(string) // 2) * 2 
+    countTo = (len(string) // 2) * 2 #used to iteratre thru string 2 char at a time
     count = 0
 
+    #loop iterates thru pairs of characters in string, converts pair to numerical val,
+    # adds it to csum, and ensures that csum does not exceed 32 bits
     while count < countTo: 
         thisVal = ord(string[count+1]) * 256 + ord(string[count]) 
         csum = csum + thisVal
         csum = csum & 0xffffffff 
         count = count + 2
 
+    #handles strings of odd lengths by processing last char separately
     if countTo < len(string):
         csum = csum + ord(string[len(string) - 1]) 
         csum = csum & 0xffffffff
 
+    #finalizes csum by shifting bits of csum 16 positions to the right (extracts upper 16 
+    # bits of csum), extracts lower 16 bits of csum using bitwise AND operation, adds upper
+    # and lower together, and adds any remaining carry
     csum = (csum >> 16) + (csum & 0xffff) 
     csum = csum + (csum >> 16)
 
+    #calculates complement of csum
     answer = ~csum
 
+    #masks to 16 bits
     answer = answer & 0xffff
  
+    #swaps byte order
     answer = answer >> 8 | (answer << 8 & 0xff00) 
     return answer
 
@@ -85,6 +93,7 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         if timeLeft <= 0:
             return "Request timed out."
 
+#constructs and sends an ICMP echo request packet
 def sendOnePing(mySocket, destAddr, ID):
     # Header is type (8), code (8), checksum (16), id (16), sequence (16)
     myChecksum = 0
@@ -92,6 +101,11 @@ def sendOnePing(mySocket, destAddr, ID):
     # Make a dummy header with a 0 checksum
  
     # struct -- Interpret strings as packed binary data
+    #creates ICMP header for echo request packet
+    #use4s struct.pack to pack values into a binary string
+    # "bbHHh" = 1 byte, 1 byte, 2 shorts, 1 short
+    #data line creates the data portion of the packet which includes time stamp in 
+    # a packed double format "d"
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1) #LOOK INTO STRUCT.PACK, b's rep bytes
     data = struct.pack("d", time.time())
 
@@ -99,36 +113,51 @@ def sendOnePing(mySocket, destAddr, ID):
     myChecksum = checksum(''.join(map(chr, header+data)))
 
     # Get the right checksum, and put in the header 
+    #checks OS (darwin is macOS) and adjusts checksum to accordingly, nothing needs to be done on windows
     if sys.platform == 'darwin':
         # Convert 16-bit integers from host to network byte order 
         myChecksum = htons(myChecksum) & 0xffff
     else:
         myChecksum = htons(myChecksum)
 
+    #header is recontructed with correct checksum (no change on windows)
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1) 
     packet = header + data
 
+    #sends packet to specified destination address
     mySocket.sendto(packet, (destAddr, 1)) # AF_INET address must be tuple, not str 
     # Both LISTS and TUPLES consist of a number of objects
     # which can be referenced by their position number within the object.
 
+#performs single round-trip ping operation by initializing raw socket, generating process ID,
+# sends ICMP echo request, receives reply, calculates round-trip time, returns delay
 def doOnePing(destAddr, timeout): 
+    #retrieves ICMP protocol number
     icmp = getprotobyname("icmp")
 
+    #creates raw socket using ICMP protocol, allows program to send/receive ICMP packets directly
     # SOCK_RAW is a powerful socket type. For more details:	http://sock-raw.org/papers/sock_raw
     mySocket = socket(AF_INET, SOCK_RAW, icmp)
 
-    myID = os.getpid() & 0xFFFF # Return the current process i 
+    #reutrns current process ID
+    #bitwise AND to ensure ID is within 16 bit range
+    #sends ping
+    #receives ping and processes packet to calculate round-trip time ("delay")
+    myID = os.getpid() & 0xFFFF
     sendOnePing(mySocket, destAddr, myID)
     delay = receiveOnePing(mySocket, myID, timeout, destAddr)
  
+    #close socket and return delay time
     mySocket.close() 
     return delay
 
+#resolves host, prints ping info, loops to send pings, prints round-trip time, returns delay of last ping
 def ping(host, timeout=1, repeat=3):
 
     # timeout=1 means: If one second goes by without a reply from the server,
     # the client assumes that either the client's ping or the server's pong is lost 
+
+    #resolves the provided host to an IP address
     dest = gethostbyname(host)
     print(f"Pinging {host} [{dest}] {repeat} times using Python:")
 
